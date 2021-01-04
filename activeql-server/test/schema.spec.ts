7e1f0024b8615f1785b0f9ecbf1dd5460312bc2a
Thomas Rekittke <betterobjects@mac.com>
2021-01-04 14:52:50 +0100

    removed non-config-entities from tests

--- activeql-server/test/schema.spec.ts
+++ activeql-server/test/schema.spec.ts
@@ -8,14 +8,6 @@ import { Entity } from '../entities/entity';
 
 describe('Schema Generation', () => {
 
-  class ATestEntity extends Entity {
-    protected getName() { return 'ATest' }
-  }
-
-  class BTestEntity extends Entity {
-    protected getName() { return 'BTest' }
-    protected getAssocTo() { return [{type: 'ATest'}]}
-  }
 
   it('should generate type and enum', async () => {
     const domainDefinition:DomainConfiguration = {
@@ -48,18 +40,6 @@ describe('Schema Generation', () => {
     expect(schema).toContain('CarFilter');
     expect(schema).toContain('Fuel');
     expect(schema).toContain('FuelFilter');
-  });
-
-  it( 'should generate schema with custom entity', async () => {
-    const domainDefinition = new DomainDefinition();
-    domainDefinition.entities.push( new ATestEntity() );
-    domainDefinition.entities.push( new BTestEntity() );
-    const runtime = await Runtime.create(  { name: 'test:schema', domainDefinition });
-    const schema = printSchema( runtime.schema );
-    expect( schema ).toContain('type ATest');
-    expect( schema ).toContain('type BTest');
-    expect( schema ).toContain('aTest: ATest');
-    // console.log( schema );
   });
 
   it('should distinguish required variants', async () => {
