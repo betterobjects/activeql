96579550d0536f3105edda6a9a6190f7b4b3cb03
Thomas Rekittke <betterobjects@mac.com>
2021-01-06 11:31:01 +0100

    explicit version for starter

--- bin/publishNg.sh
+++ bin/publishNg.sh
@@ -5,10 +5,18 @@
 ROOT=`pwd`
 
 cd $ROOT/angular/projects/activeql-admin-ui
-npm version patch
+VERSION=`npm version patch`
 
 cd $ROOT/angular
 ng build activeql-admin-ui --prod
 
 cd $ROOT/angular/dist/activeql-admin-ui
 npm publish
+
+rm -rf $ROOT/angular/dist
+
+sleep 5
+
+cd $ROOT/../activeql-starter/angular
+npm i activeql-admin-ui@$VERSION
+
--- bin/publishServer.sh
+++ bin/publishServer.sh
@@ -5,9 +5,12 @@
 ROOT=`pwd`
 
 cd $ROOT/activeql-server
-npm version patch
+VERSION=`npm version patch`
+
 npm publish         # this will build 1st in dist
 rm -rf dist         # otherwise you won't see changes in the ts-files in the running application
 
+sleep 5
+
 cd $ROOT/../activeql-starter/express
-npm i activeql-server               # this will bumb to the latest version
+npm i activeql-server@$VERSION
