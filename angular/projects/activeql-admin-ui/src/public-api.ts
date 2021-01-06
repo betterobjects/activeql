12fc3d8f9057f34939d98d384e7ba1e7c96c87a6
Thomas Rekittke <betterobjects@mac.com>
2021-01-06 11:01:56 +0100

    added some more components / services to public API

--- angular/projects/activeql-admin-ui/src/public-api.ts
+++ angular/projects/activeql-admin-ui/src/public-api.ts
@@ -4,15 +4,22 @@
 
 export * from './lib/activeql-admin-ui.module';
 export * from './lib/activeql-admin-routing.module';
-export * from './lib/components/error.component';
+export * from './lib/components/admin.component';
+export * from './lib/components/admin-action.component';
 export * from './lib/components/index/index.component';
-export * from './lib/components/confirm-dialog/confirm-dialog.component';
 export * from './lib/components/create/create.component';
-export * from './lib/components/edit/edit.component';
-export * from './lib/components/form/form.component';
 export * from './lib/components/show/show.component';
+export * from './lib/components/edit/edit.component';
+export * from './lib/components/error.component';
+export * from './lib/components/form/form.component';
 export * from './lib/components/message-dialog/message-dialog.component';
 export * from './lib/components/table/table.component';
 export * from './lib/components/dynamic-content.component';
-export * from './lib/services/admin-config.service';
+export * from './lib/components/file-upload.component';
+export * from './lib/components/message-dialog/message-dialog.component';
+export * from './lib/components/confirm-dialog/confirm-dialog.component';
 
+export * from './lib/components/breadcumb.component';
+
+export * from './lib/services/admin-config.service';
+export * from './lib/services/admin-data.service';
