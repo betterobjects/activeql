bac7489c95cfe439e9340f8a1fe3009a3bbd7dd8
Thomas Rekittke <betterobjects@mac.com>
2021-01-11 23:35:34 +0100

    added label ressources

--- angular/src/app/config/admin.config.ts
+++ angular/src/app/config/admin.config.ts
@@ -2,14 +2,26 @@ import _ from 'lodash';
 import { AdminConfig } from 'activeql-admin-ui';
 
 export const adminConfig:AdminConfig = {
+  locale: 'en',
   entities:  {
     Car: {
       asLookup: {
         query: () => ({id: true, licence:true } ),
         render: (item:any) => _.get(item, 'licence') },
       index: {
-        fields: ['id', 'color', 'brand', 'createdAt'],
+        fields: ['vin', 'licence', 'color', 'brand', 'createdAt'],
         search: true
+      }
+    }
+  },
+  resources: {
+    en: {
+      label: {
+        entities: {
+          Driver: {
+            licenceValid: "Licence valid thru"
+          }
+        }
       }
     }
   }
