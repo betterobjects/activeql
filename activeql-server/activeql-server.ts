28cfdf19c8f9ce09a15dc32ae5a1181ca33cca23
Thomas Rekittke <betterobjects@mac.com>
2021-01-14 15:05:28 +0100

    make apolloConfig optional

--- activeql-server/activeql-server.ts
+++ activeql-server/activeql-server.ts
@@ -1,9 +1,10 @@
 import { ApolloServer, ApolloServerExpressConfig } from 'apollo-server-express';
+import depthLimit from 'graphql-depth-limit';
 import _ from 'lodash';
 
-import { DomainDefinition } from './core/domain-definition';
 import { DomainConfiguration } from './core/domain-configuration';
-import { RuntimeConfig, Runtime } from './core/runtime';
+import { DomainDefinition } from './core/domain-definition';
+import { Runtime, RuntimeConfig } from './core/runtime';
 
 /**
  * I'd love to have this outside of this (library) package - alas Apollo checks somehow that the same constructor
@@ -11,8 +12,8 @@ import { RuntimeConfig, Runtime } from './core/runtime';
  */
 export class ActiveQLServer {
 
-  static async create( apolloConfig:ApolloServerExpressConfig = {}, runtimeConfig?:RuntimeConfig|DomainDefinition|DomainConfiguration|string ):Promise<ApolloServer> {
-    if( ! runtimeConfig ) runtimeConfig = './domain-configuration';
+  static async create( runtimeConfig:RuntimeConfig|DomainDefinition|DomainConfiguration|string, apolloConfig?:ApolloServerExpressConfig ):Promise<ApolloServer> {
+    if( ! apolloConfig ) apolloConfig = { validationRules: [depthLimit(7)] }
     const runtime = await Runtime.create( runtimeConfig );
     apolloConfig.schema = runtime.schema;
     apolloConfig.context = async (expressContext:any) => {
