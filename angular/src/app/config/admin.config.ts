794f3168997ad44336d58b08bcb091ba1f242b9e
Thomas Rekittke <betterobjects@mac.com>
2021-01-15 10:07:34 +0100

    example config

--- angular/src/app/config/admin.config.ts
+++ angular/src/app/config/admin.config.ts
@@ -4,22 +4,18 @@ import { AdminConfig } from 'activeql-admin-ui';
 export const adminConfig:AdminConfig = {
   locale: 'en',
   entities:  {
-    _Car: {
+    Car: {
       asLookup: {
         query: () => ({id: true, licence:true } ),
         render: (item:any) => _.get(item, 'licence') },
-      index: {
-        fields: ['vin', 'licence', 'color', 'brand', 'createdAt'],
-        search: true
-      }
     }
   },
   resources: {
     en: {
       label: {
         entities: {
-          Driver: {
-            licenceValid: "Licence valid thru"
+          Car: {
+            licence: "Licence Plate"
           }
         }
       }
